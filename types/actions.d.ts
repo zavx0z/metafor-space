7bb4f01453b68a28d54a2613ba57d6a2529a5bc9
zavx0z <metaversebdfl@gmail.com>
2025-04-14 01:05:03 +0300

    Обновлены аннотации типов в файле actions.d.ts. Добавлены комментарии для улучшения понимания структуры действий и чистых действий. Уточнены свойства и их описание для типов Action и ActionClean.

--- types/actions.d.ts
+++ types/actions.d.ts
@@ -2,13 +2,28 @@ import type { ContextData, ContextDefinition, Update } from "./context.d.ts"
 import type { Core } from "./core.d.ts"
 
 /**
- Действие
+ Действия объявленные в конструкторе Meta в actions
+
+ Предпочтительно не использовать чистые действия, а объявлять их в transitions.
+
+ @template C - Тип данных контекста
+ @template I - Тип внутренних данных
+
+ @property [key: string]: Action<C, I> - Действие
+ */
+export type Actions<C extends ContextDefinition, I extends Record<string, unknown>> = { [key: string]: Action<C, I> }
+
+/**
+ Действие объявленное в конструкторе Meta в actions
+
+ Может хранить и получать данные из core
+ Также может получать доступ к сервисам core
 
  @template C - Тип данных контекста
  @template I - Тип внутренних данных
  @property context - Данные контекста
  @property update - Функция обновления контекста
- @property core - Внутренние данные
+ @property core - Внутренние данные и сервисы
  */
 export type Action<C extends ContextDefinition, I extends Record<string, unknown>> = ({
   context,
@@ -20,26 +35,19 @@ export type Action<C extends ContextDefinition, I extends Record<string, unknown
 }) => void | Promise<void>
 
 /**
- Чистое действие
+ Чистое действие объявленное в transitions
+
+ Не имеет доступа к core
+ Является чистой функцией
 
  @template C - Тип данных контекста
  @property context - Данные контекста
  @property update - Функция обновления контекста
  */
- export type ActionClean<C extends ContextDefinition> = ({
+export type ActionClean<C extends ContextDefinition> = ({
   context,
   update,
 }: {
   context: ContextData<C>
   update: Update<C>
 }) => void | Promise<void>
-
-/**
- Действия
-
- @template C - Тип данных контекста
- @template I - Тип внутренних данных
-
- @property [key: string]: Action<C, I> - Действие
- */
-export type Actions<C extends ContextDefinition, I extends Record<string, unknown>> = { [key: string]: Action<C, I> }
