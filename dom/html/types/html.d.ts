4a0470df66e40b495d5551b21a26337cf2c346ee
zavx0z <metaversebdfl@gmail.com>
2025-05-31 15:12:56 +0700

    Добавлены новые типы SanitizerFactory и ValueSanitizer в html.d.ts для улучшения безопасности при записи в DOM. Удален устаревший файл global.d.ts, содержащий аналогичные декларации. Исправлен импорт Directive в html.d.ts для корректной работы с типами.

--- dom/html/types/html.d.ts
+++ dom/html/types/html.d.ts
@@ -1,4 +1,4 @@
-import type {Directive} from "./directive.ts"
+import type {Directive} from "../directive.d.ts"
 import {HTML_RESULT, MATHML_RESULT, SVG_RESULT, type Template} from "../html"
 
 export type ResultType = typeof HTML_RESULT | typeof SVG_RESULT | typeof MATHML_RESULT
