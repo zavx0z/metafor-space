a183af10427dc82eef65e69c464dd83124d9e444
zavx0z <metaversebdfl@gmail.com>
2025-06-01 09:20:51 +0700

    Обновлен код в nodes.js и main.js: улучшена читаемость за счет упрощения структуры и форматирования. Исправлены названия полей в контексте, добавлены новые элементы для отображения состояния. Обновлены функции обработки состояний и действий, добавлены выводы в консоль для отладки.

--- nodes/dev/main.js
+++ nodes/dev/main.js
@@ -1,55 +1,41 @@
 import {MetaFor} from "../../metafor.js"
 import {Nodes} from "../nodes.js"
 
-const nodes = Nodes.create({
+Nodes.create({
   state: "ожидание патча",
 })
 
-MetaFor("nodes", {
-  description: "Nodes",
-  development: false,
+MetaFor("node", {description: "Nodes", development: false}).states(
+  "начало",
+  "конец"
+).context((t) => ({
+  status: t.enum("start", "end")({title: "Status", default: "start"}),
+})).core(() => ({})
+).transitions([
+  {
+    from: "начало",
+    action: ({context}) => {
+      console.log(context)
+    },
+    to: [{state: "конец", when: {status: "start"}}],
+  },
+  {
+    from: "конец",
+    action: async () => {
+      console.log("конец")
+    },
+    to: [{state: "начало", when: {status: "end"}}],
+  },
+]).view({
+  render: ({html, context}) => html`
+      <h1>${context.status}</h1>
+  `
+}).create({
+  state: "начало",
+  onTransition: (preview, current, meta) => {
+    console.log(preview, current, meta)
+  },
+  onUpdate: (value) => {
+    console.log(value)
+  },
 })
-  .states("начало", "конец")
-  .context((t) => ({
-    status: t.enum("start", "end")({title: "Status", default: "start"}),
-  }))
-  .core(() => ({}))
-  .transitions([
-    {
-      from: "начало",
-      action: ({context}) => {
-        console.log(context)
-      },
-      to: [
-        {
-          state: "конец",
-          when: {
-            status: "start",
-          },
-        },
-      ],
-    },
-    {
-      from: "конец",
-      action: async () => {
-        console.log("конец")
-      },
-      to: [
-        {
-          state: "начало",
-          when: {
-            status: "end",
-          },
-        },
-      ],
-    },
-  ])
-  .create({
-    state: "начало",
-    onTransition: (preview, current, meta) => {
-      console.log(preview, current, meta)
-    },
-    onUpdate: (value) => {
-      console.log(value)
-    },
-  })
--- nodes/nodes.js
+++ nodes/nodes.js
@@ -1,74 +1,70 @@
 import {MetaFor} from "../metafor.js"
 import ELK from "elkjs"
 
-export const Nodes = MetaFor("nodes", {
-  description: "Nodes",
-  development: true,
-})
-  .states("ожидание патча", "добавление ноды", "удаление ноды")
-  .context((t) => ({
-    op: t.enum("add", "remove")({title: "Operation", nullable: true}),
-    nodes: t.array({title: "Коллекция meta", default: []}),
-  }))
-  .core(() => ({
-    elk: new ELK(),
-  }))
-  .transitions([
-    {
-      from: "ожидание патча",
-      action: ({context}) => {
-        console.log(context)
-      },
-      to: [
-        {state: "добавление ноды", when: {op: "add"}},
-        {state: "удаление ноды", when: {op: "remove"}},
-      ],
+export const Nodes = MetaFor("nodes", {description: "Nodes", development: true}
+).states(
+  "ожидание патча",
+  "добавление ноды",
+  "удаление ноды"
+).context((t) => ({
+  op: t.enum("add", "remove")({title: "Тип патча", nullable: true}),
+  nodes: t.array({title: "Коллекция meta", default: []}),
+})).core(() => ({
+  elk: new ELK(),
+})).transitions([
+  {
+    from: "ожидание патча",
+    action: ({context}) => {
+      console.log(context)
     },
-    {
-      from: "добавление ноды",
-      action: ({context, update}) => {
-        console.log(context)
-        update({op: null})
-      },
-      to: [{state: "ожидание патча", when: {op: null}}],
+    to: [
+      {state: "добавление ноды", when: {op: "add"}},
+      {state: "удаление ноды", when: {op: "remove"}},
+    ],
+  },
+  {
+    from: "добавление ноды",
+    action: ({context, update}) => {
+      console.log(context)
+      update({op: null})
     },
-    {
-      from: "удаление ноды",
-      action: ({context, update}) => {
-        console.log(context)
-        update({op: null, nodes: []})
-      },
-      to: [{state: "ожидание патча", when: {op: null}}],
+    to: [{state: "ожидание патча", when: {op: null}}],
+  },
+  {
+    from: "удаление ноды",
+    action: ({context, update}) => {
+      console.log(context)
+      update({op: null, nodes: []})
     },
-  ])
-  .reactions([
-    {
-      op: "add",
-      action: ({context, patch, update}) => {
-        console.log(patch)
-        update({nodes: [...context.nodes, patch.value]})
-      },
+    to: [{state: "ожидание патча", when: {op: null}}],
+  },
+]).reactions([
+  {
+    op: "add",
+    action: ({context, patch, update}) => {
+      console.log(patch)
+      update({nodes: [...context.nodes, patch.value]})
     },
-  ])
-  .view({
-    render: ({html, update, context}) => html`
-        <div>
-            <h1>Nodes</h1>
-            <button @click=${() => update({op: "add"})}>Add</button>
-            <button @click=${() => update({op: "remove"})}>Remove</button>
-            ${context.nodes.map(/**@param {{id: string}} i**/(i) => html`
-                <div>${i.id}</div>
-            `)}
-        </div>
-    `,
-    style: ({css}) => css`
-        :host {
-            color: white;
-        }
+  },
+]).view({
+  render: ({html, update, context}) => html`
+      <div>
+          <h1>Nodes</h1>
+          <button @click=${() => update({op: "add"})}>Add</button>
+          <button @click=${() => update({op: "remove"})}>Remove</button>
+          ${context.nodes.map(/**@param {{id: string}} i**/(i) => html`
+              <div>${i.id}</div>
+          `)}
+      </div>
+  `,
+  style: ({css}) => css`
+      :host {
+          color: white;
+      }
 
-        h1,
-        p {
-            color: red;
-        }
-    `,
-  })
+      h1,
+      p {
+          color: red;
+      }
+  `,
+})
