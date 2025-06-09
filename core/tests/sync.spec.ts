26bce5e3d194f94ec5e812e8f28c2a9a5e0583fa
zavx0z <metaversebdfl@gmail.com>
2025-06-09 20:34:15 +0700

    Добавлена новая команда тестирования для transitions.lock.spec.ts в package.json. Обновлены тесты в sync.spec.ts и transitions.lock.spec.ts: улучшена логика переходов и синхронизации данных, добавлены новые состояния и условия для триггеров. Исправлены ссылки на экземпляры мета-объектов.

--- core/tests/sync.spec.ts
+++ core/tests/sync.spec.ts
@@ -1,46 +1,71 @@
 import {describe, expect, test} from "bun:test"
 import {MetaFor, type Meta} from "@metafor/space"
-
+// FIXME:
 describe("Синхронизация core и context", async () => {
   document.body.innerHTML = `<metafor-test></metafor-test>`
 
   const Meta = MetaFor("test")
-    .states("IDLE")
+    .states("IDLE", "push", "pop")
     .context((t) => ({
+      process: t.enum("pop", "push")({nullable: true}),
       dataLength: t.number({default: 0}),
     }))
     .core(({update}) => ({
       data: [],
       popData() {
-        update({dataLength: 0})
         this.data.splice(0, this.data.length)
+        update({dataLength: this.data.length, process: null})
+
       },
       pushData(data: any) {
         this.data.push(data)
-        update({dataLength: this.data.length})
+        update({dataLength: this.data.length, process: null})
       },
     }))
-    .transitions([])
+    .transitions([
+      {
+        from: "IDLE",
+        to: [
+          {state: "push", when: {process: "push"}},
+          {state: "pop", when: {process: "pop"}}
+        ]
+      },
+      {
+        from: "push",
+        action: ({core}) => core.pushData(),
+        to: [
+          {state: "IDLE", when: {process: null}},
+          {state: "pop", when: {process: "pop"}}
+        ]
+      },
+      {
+        from: "pop",
+        action: ({core}) => core.popData(),
+        to: [{state: "IDLE", when: {process: null}}]
+      }
+    ])
     .create({state: "IDLE"})
   const meta = document.querySelector('metafor-test') as Meta<typeof Meta.state, typeof Meta.context>
 
   let count = 50
   const delay = 20
   const interval = setInterval(() => {
-    // meta.core.pushData(count)
+    meta.update({process: "push"})
     count--
     if (count === 0) clearInterval(interval)
   }, delay)
 
   meta.onUpdate((values) => {
-    if (values.dataLength > 4) {
+    // console.log("upd", values)
+    if (values.dataLength > 47) {
       test("Данные ядра синхронизируются с контекстом", () => {
         // meta.core.popData()
+        meta.update({process: "pop"})
         // Simulate heavy synchronous computation (~1 second)
-        let result = 0
-        for (let i = 0; i < 100_000_000; i++) {
-          result += Math.sin(i) * Math.cos(i)
-        }
+        // let result = 0
+        // for (let i = 0; i < 100_000_000; i++) {
+        //   result += Math.sin(i) * Math.cos(i)
+        // }
         expect(meta.context.dataLength).toBe(0)
         // expect(meta.core.data.length).toBe(0)
       })
--- core/tests/transitions.lock.spec.ts
+++ core/tests/transitions.lock.spec.ts
@@ -1,72 +1,78 @@
-import { expect, test } from "bun:test"
-import { MetaFor } from "@metafor/space"
+import {expect, test} from "bun:test"
+import {MetaFor, type Meta} from "@metafor/space"
 
 test("Блокировка переходов перед входом в новое состояние", async () => {
   let value = -1
-
-  const meta = MetaFor("test-sync")
+  document.body.innerHTML = `<metafor-test-1></metafor-test-1>`
+  const Meta = MetaFor("test-1")
     .states("INIT", "PROCESS", "DONE")
     .context((t) => ({
-      value: t.number({ nullable: true }),
+      value: t.number({nullable: true}),
     }))
     .core()
     .transitions([
       {
         from: "INIT",
-        action: ({ update }) => {
+        action: ({update}) => {
           update({value: 11})
         },
-        to: [{ state: "PROCESS", when: { value: { gt: 10 } } }],
+        to: [{state: "PROCESS", when: {value: {gt: 10}}}],
       },
       {
         from: "PROCESS",
-        action: ({ update }) => {
-          update({ value: 15 })
+        action: ({update}) => {
+          update({value: 15})
           const end = Date.now() + 500
           while (Date.now() < end) {
             // Блокируем поток
           }
         },
         to: [
-          { state: "DONE", when: { value: { gt: 14 } } },
-          { state: "INIT", when: { value: { lt: 4 } } },
+          {state: "DONE", when: {value: {gt: 14}}},
+          {state: "INIT", when: {value: {lt: 4}}},
         ],
       },
     ])
     .create({
       state: "INIT",
       onTransition: async (_, newState, meta) => {
+        console.log(newState)
         if (newState === "PROCESS") {
-          // meta.update({ value: 1 }) // не должен вызвать переход, но контекст должен быть обновлен даже при блокировке переходов
+          const meta = document.querySelector('metafor-test-1') as Meta<typeof Meta.state, typeof Meta.context>
+          meta.update({ value: 1 }) // не должен вызвать переход, но контекст должен быть обновлен даже при блокировке переходов
           value = meta.context.value
         }
       },
     })
+  const meta = document.querySelector('metafor-test-1') as Meta<typeof Meta.state, typeof Meta.context>
+
   await Bun.sleep(1000)
   expect(value).toBe(1)
   expect(meta.state).toBe("DONE")
 })
 
 test("Блокировка переходов для асинхронного действия", async () => {
-  const meta = MetaFor("test-async")
+  document.body.innerHTML = `<metafor-test-2></metafor-test-2>`
+  const Meta = MetaFor("test-2")
     .states("INIT", "PROCESS", "DONE")
     .context((t) => ({
-      value: t.number({ nullable: true }),
+      value: t.number({nullable: true}),
     }))
     .core()
     .transitions([
       {
         from: "INIT",
-        action: async ({ update }) => {
+        action: async ({update}) => {
           await new Promise((resolve) => setTimeout(resolve, 10))
-          update({ value: 15 }) // Это не должно вызвать переход
+          update({value: 15}) // Это не должно вызвать переход
         },
-        to: [{ state: "DONE", when: { value: { gt: 10 } } }],
+        to: [{state: "DONE", when: {value: {gt: 10}}}],
       },
     ])
-    .create({ state: "INIT" })
+    .create({state: "INIT"})
+  const meta = document.querySelector('metafor-test-2') as Meta<typeof Meta.state, typeof Meta.context>
 
-  const result: unknown = meta.update({ value: 1 })
+  const result: unknown = meta.update({value: 1})
   if (result instanceof Promise) {
     expect(meta.process).toBe(true)
     await result
@@ -76,23 +82,25 @@ test("Блокировка переходов для асинхронного д
 })
 
 test("Снятие блокировки после действия", async () => {
-  const meta = MetaFor("test-lock")
+  document.body.innerHTML = `<metafor-test-3></metafor-test-3>`
+  const Meta = MetaFor("test-3")
     .states("INIT", "DONE")
     .context((t) => ({
-      value: t.number({ nullable: true, default: 2 }),
+      value: t.number({nullable: true, default: 2}),
     }))
     .core()
     .transitions([
       {
         from: "INIT",
-        action: async ({ update }) => {
+        action: async ({update}) => {
           await new Promise((resolve) => setTimeout(resolve, 50))
-          update({ value: 15 })
+          update({value: 15})
         },
-        to: [{ state: "DONE", when: { value: { gt: 10 } } }],
+        to: [{state: "DONE", when: {value: {gt: 10}}}],
       },
     ])
-    .create({ state: "INIT"})
+    .create({state: "INIT"})
+  const meta = document.querySelector('metafor-test-3') as Meta<typeof Meta.state, typeof Meta.context>
 
   expect(meta.state).toBe("INIT")
   expect(meta.process).toBe(true)
