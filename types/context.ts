b6fef6c71a5b15ecf51a375e1b2dd5b9453f6874
zavx0z <metaversebdfl@gmail.com>
2025-06-24 10:37:09 +0700

    Обновлена логика определения типов в context.ts с учетом параметра "nullable". Теперь типы данных корректно обрабатывают возможность наличия null в зависимости от значения свойства "nullable". Оптимизирована структура типов для повышения читаемости и согласованности кода.

--- types/context.ts
+++ types/context.ts
@@ -38,10 +38,16 @@ export type TypeDefinition =
     [K in keyof C]: C[K] extends ArrayDefinition
         ? any[]
         : C[K] extends EnumDefinition<infer T>
-            ? T[number] | null
+            ? C[K]["nullable"] extends true
+                ? T[number] | null
+                : T[number]
             : C[K] extends TypeDefinition
-                ? ExtractTypeValue<C[K]> | null
-                : never
+                ? "nullable" extends keyof C[K]
+                    ? C[K]["nullable"] extends true
+                        ? ExtractTypeValue<C[K]> | null
+                        : ExtractTypeValue<C[K]>
+                    : ExtractTypeValue<C[K]>
+                : ExtractTypeValue<C[K]>
 }
 
 /**
